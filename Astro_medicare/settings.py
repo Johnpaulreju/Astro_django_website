8740a9262da3e1d71ef023bf7d60cfba849cae0d
Johnpaul Reju <79637117+Johnpaulreju@users.noreply.github.com>
2025-03-31 16:34:03 +0530

    Update settings.py

--- Astro_medicare/settings.py
+++ Astro_medicare/settings.py
@@ -62,8 +62,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "projectmaker2k@gmail.com"
-EMAIL_HOST_PASSWORD = "qssz rwcz mobl uhib"
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
 
 
 
